963166a990ccbb0b74fc59b022bab5a463914279
RaphaelFreitasDEVFull <freitasraphael.dev@gmail.com>
2025-05-17 22:33:12 -0300

    fix: unused var

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -2,17 +2,6 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import { Languages } from "lucide-react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 const Sidebar = () => {
   return (
